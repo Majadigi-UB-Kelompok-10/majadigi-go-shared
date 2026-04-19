ea5e4a5bc96dd890dae34a8c5a1408bc02e120cb
Alfaril Dzaky Praptana <alfarildzaky@gmail.com>
2026-04-19 10:41:14 +0700

    fix: update AutoRegister function to use pgxpool for database connection

--- shared/registry/gateway.go
+++ shared/registry/gateway.go
@@ -7,7 +7,19 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-func AutoRegister(db *pgxpool.Pool, slugName string, pageURL string) {
+func AutoRegister(gatewayDBUrl string, slugName string, pageURL string) {
+	if gatewayDBUrl == "" {
+		log.Println("⚠️ AutoRegister dibatalkan: URL DB Gateway tidak ditemukan")
+		return
+	}
+
+	dbGateway, err := pgxpool.New(context.Background(), gatewayDBUrl)
+	if err != nil {
+		log.Printf("❌ Gagal konek ke DB Gateway: %v\n", err)
+		return
+	}
+	defer dbGateway.Close()
+
 	query := `
 		INSERT INTO endpoint_list (slug_name, page_url) 
 		VALUES ($1, $2) 
@@ -15,10 +27,10 @@ func AutoRegister(db *pgxpool.Pool, slugName string, pageURL string) {
 		DO UPDATE SET page_url = EXCLUDED.page_url;
 	`
 
-	_, err := db.Exec(context.Background(), query, slugName, pageURL)
+	_, err = dbGateway.Exec(context.Background(), query, slugName, pageURL)
 	if err != nil {
-		log.Printf("Gagal mendaftarkan '%s': %v\n", slugName, err)
+		log.Printf("❌ Gagal mendaftarkan '%s': %v\n", slugName, err)
 	} else {
-		fmt.Printf("Berhasil! Gateway merouting '/%s/*' ke '%s'\n", slugName, pageURL)
+		fmt.Printf("✅ Berhasil! Service '%s' mendaftarkan diri ke Gateway\n", slugName)
 	}
 }
